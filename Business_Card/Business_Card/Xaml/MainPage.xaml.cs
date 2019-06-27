7ad629760750e73da06e935201dda78e72a2c585
GarethPGH <garethlwarren@gmail.com>
2019-06-26 20:35:18 -0400

    Beginning to feel like I have no idea what I am doing
    I made some changes. Just trying to get some mock data to show up on the stupid app to know it is getting data. Mock data is not showing up in the page. Of course.

--- Business_Card/Business_Card/Xaml/MainPage.xaml.cs
+++ Business_Card/Business_Card/Xaml/MainPage.xaml.cs
@@ -3,18 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Business_Card.ViewModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Business_Card.Model;
 
-namespace Business_Card.Xaml
+namespace Business_Card
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
-            InitializeComponent();
+            try
+            {
+                BindingContext = new Business_Card.Model.MockDataClass();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error Binding Context has not been assigned to", e);
+            }
+            
         }
     }
 }
---
+++ Business_Card/ViewModel/TestBusinessCardViewModelClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+using Business_Card.Model;
+
+//Code here will be edited and then copied into BusinessCardViewModel if it works
+//Code copied from MSDN Xamarin Commanding docs 
+public class TestBusinessCardViewModel : INotifyPropertyChanged
+{
+    TestBusinessCardViewModel businesscardEdit;
+    bool isEditing;
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public bool IsEditing
+    {
+        private set { SetProperty(ref isEditing, value); }
+        get { return isEditing; }
+    }
+   
+    public ICommand NewCommand { private set; get; }
+
+    public ICommand SubmitCommand { private set; get; }
+
+    public ICommand CancelCommand { private set; get; }
+
+    public ObservableCollection<TestBusinessCardViewModel> Cards { get; } = new ObservableCollection<TestBusinessCardViewModel>();
+
+    bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+    {
+        if (Object.Equals(storage, value))
+            return false;
+
+        storage = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
